e2961597a17e4b685d9a75f0f6b2149af6b58ce3
ram <ramsharan0230@gmail.com>
2025-07-07 10:24:07 +0100

    Feature: Pagination features implemented

--- app/Http/Controllers/ProductServiceController.php
+++ app/Http/Controllers/ProductServiceController.php
@@ -13,7 +13,28 @@ class ProductServiceController extends Controller
 {
     public function fetchProducts(Request $request): JsonResponse
     {
-        $payload = ProductResource::collection(Product::all());
-        return ApiResponse::success($payload, "Products fetched successfully.", Response::HTTP_OK);
+        if ($request->query('paginate') === 'false') {
+            $products = Product::all();
+
+            return ApiResponse::success([
+                'products' => ProductResource::collection($products),
+                'pagination' => null, // or just omit this key if not needed
+            ], "Products fetched successfully.", Response::HTTP_OK);
+        }
+
+        $perPage = $request->input('per_page', 10);
+        $page = $request->input('page', 1);
+
+        $products = Product::paginate($perPage, ['*'], 'page', $page);
+
+        return ApiResponse::success([
+            'products' => ProductResource::collection($products),
+            'pagination' => [
+                'total' => $products->total(),
+                'per_page' => $products->perPage(),
+                'current_page' => $products->currentPage(),
+                'last_page' => $products->lastPage(),
+            ]
+        ], "Products fetched successfully.", Response::HTTP_OK);
     }
 }
